78dc6ed63c801d816c0efcdde70b219e43d4bbc0
IKRAMCHAAIBI <chaaibi.ikrame@gmail.com>
2024-04-02 16:19:24 -0700

    2-square.py

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+""Square module.""
+claas Square:
+    ""Defines a aquare.""
+    def __init__(self, size):
+        ""Constructor. Args: size: length of side of the square.""
+        self.__size = size
